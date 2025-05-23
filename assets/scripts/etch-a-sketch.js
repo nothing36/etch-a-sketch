99d53de76953d4a29c7395f5ab3eefca1fbdc7c2
Paul <53634603+nothing36@users.noreply.github.com>
2025-05-23 15:58:17 -0600

    Add toggle shape funcitonality

--- assets/scripts/etch-a-sketch.js
+++ assets/scripts/etch-a-sketch.js
@@ -1,12 +1,28 @@
-const boardLen = 16
+const boardLen = 50
+const squareSize = "800px";
+let isSquare = false;
+let itemSize = 100 / boardLen
 const container = document.querySelector(".container")
 
 // fill window with boxes on load
 window.onload = (event) => {
   for (i = 0; i < (boardLen * boardLen); i++) {
     let etchBox = document.createElement("div")
-    etchBox.style.cssText = "background-color: white; border: 1px solid black; width: 50px; height: 50px;"
+    etchBox.style.cssText = `box-sizing: border-box; background-color: white; border: 1px solid black; flex: 0 0 calc(${itemSize}%);`
 
     container.appendChild(etchBox)
   }
 };
+
+// toggle square or fill sketch board
+function toggleShape() {
+    if (!isSquare) {
+        container.style.height = squareSize
+        container.style.width = squareSize
+        isSquare = true;
+    } else {
+        container.style.height = "100vh"
+        container.style.width = "100vw"
+        isSquare = false;
+    }
+}
