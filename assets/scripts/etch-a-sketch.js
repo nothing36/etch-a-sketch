017919d845601dd5ff2609c350121202ec1b9211
Paul <53634603+nothing36@users.noreply.github.com>
2025-05-24 11:21:18 -0600

    Add color changing funcitonality

--- assets/scripts/etch-a-sketch.js
+++ assets/scripts/etch-a-sketch.js
@@ -2,18 +2,27 @@ const boardLen = 100
 const squareSize = "calc(100vh - 40px)";
 let isSquare = false;
 let itemSize = 100 / boardLen
+let fillColor = "red"
 const container = document.querySelector(".container")
 const toolbar = document.querySelector(".toolbar");
 
 // fill window with boxes on load
 window.onload = (event) => {
+  buildGrid()
+};
+
+// build grid function
+function buildGrid() {
+  container.innerHTML = '';   // empty the grid container
+
+  // fill the grid
   for (i = 0; i < (boardLen * boardLen); i++) {
     let etchBox = document.createElement("div")
     etchBox.style.cssText = `box-sizing: border-box; background-color: white; border: 1px solid black; flex: 0 0 calc(${itemSize}%);`
 
     container.appendChild(etchBox)
   }
-};
+}
 
 // toggle square or fill sketch board
 function toggleShape() {
@@ -31,7 +40,7 @@ function toggleShape() {
 // hover functionality
 container.addEventListener("mouseover", function(event) {
     const hoverElement = event.target
-    hoverElement.style.backgroundColor = "black";
+    hoverElement.style.backgroundColor = fillColor;
 })
 
 // toolbar functionality
@@ -41,9 +50,22 @@ toolbar.addEventListener('click', function(event) {
   if (btnClicked) {
     switch (btnClicked.id) {
       case "btnNewGrid":
+        buildGrid()
         break
       case "btnShape":
         toggleShape()
+        break
+      case "btnRainbow":
+        break
+      case "btnRed":
+        fillColor = "red"
+        break
+      case "btnGreen":
+        fillColor = "green"
+        break
+      case "btnBlue":
+        fillColor = "blue"
+        break
     }
   }
 })
